86c53dc7f13bdef2835319c34d907e9d4618b464
aswinmohanme <dev.aswinmohan@gmail.com>
2022-12-08 14:17:08 +0530

    Let there be light

---
+++ config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :blog, Blog.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "blog_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :blog, BlogWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "yL3bL37uGN5TyfebEt40twR14CXRvscHD39CkX4DHXPFy2y3uF4bKKFAE2i/ScaI",
+  server: false
+
+# In test we don't send emails.
+config :blog, Blog.Mailer, adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
