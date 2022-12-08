34131258a67748fe59c140e6c6b34588b973e156
aswinmohanme <dev.aswinmohan@gmail.com>
2022-12-08 14:23:50 +0530

    Setup TailwindCSS

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :blog, BlogWeb.Endpoint,
   secret_key_base: "ccofO+7rMh+SKyx6p+XNOVCWkZmfGwJQKNYu/Aj32JurcpVpfh4bpW8asuboOinO",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
