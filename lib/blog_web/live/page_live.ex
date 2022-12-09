dadcb6457aea42a053ddd67371c5096b03067fa5
aswinmohanme <dev.aswinmohan@gmail.com>
2022-12-09 16:30:15 +0530

    Live navigation with tags

--- lib/blog_web/live/page_live.ex
+++ lib/blog_web/live/page_live.ex
@@ -3,9 +3,41 @@ defmodule BlogWeb.PageLive do
 
   alias Blog.Posts
 
-  def render(%{live_action: :index} = assigns) do
+  @impl true
+  def mount(%{}, _, %{assigns: %{live_action: :index}} = socket) do
     posts = Posts.list_posts()
-    Phoenix.View.render(BlogWeb.PageView, "index.html", assigns |> Map.put(:posts, posts))
+    tags = Posts.list_tags()
+    {:ok, socket |> assign(posts: posts, tags: tags)}
+  end
+
+  def mount(_, _, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(%{"tag" => tag}, _, %{assigns: %{live_action: :index}} = socket) do
+    posts = Posts.list_posts_by_tag!(tag)
+    {:noreply, socket |> assign(posts: posts)}
+  end
+
+  @impl true
+  def handle_params(%{}, _, %{assigns: %{live_action: :index}} = socket) do
+    posts = Posts.list_posts()
+    {:noreply, socket |> assign(posts: posts)}
+  end
+
+  @impl true
+  def handle_params(_, _, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def render(%{live_action: :index} = assigns) do
+    Phoenix.View.render(
+      BlogWeb.PageView,
+      "index.html",
+      assigns
+    )
   end
 
   def render(%{live_action: :about} = assigns) do
