4c9a36b95fec59d943c7a4e78dbf8ef499c0025e
aswinmohanme <dev.aswinmohan@gmail.com>
2022-12-15 12:30:21 +0530

    Add description on page index

--- lib/blog_web/live/page_live.ex
+++ lib/blog_web/live/page_live.ex
@@ -7,7 +7,16 @@ defmodule BlogWeb.PageLive do
   def mount(%{}, _, %{assigns: %{live_action: :index}} = socket) do
     posts = Posts.list_posts()
     tags = Posts.list_tags()
-    {:ok, socket |> assign(posts: posts, tags: tags)}
+
+    meta_attrs = [
+      %{
+        name: "description",
+        content:
+          "Hey, I'm Aswin Mohan. I'm a Software Developer who works across frontend, backend, mobile and design. I am currently based out of Kerala, India. I love to design, code and make bold, beautiful, useful things."
+      }
+    ]
+
+    {:ok, socket |> assign(posts: posts, tags: tags, meta_attrs: meta_attrs)}
   end
 
   def mount(_, _, socket) do
