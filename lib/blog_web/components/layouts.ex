c2558742e59de785ad6f18c3542b1870e3ef7a13
aswinmohanme <dev.aswinmohan@gmail.com>
2023-10-29 14:26:38 +0530

    Migrate from deprecated redirects to links

--- lib/blog_web/components/layouts.ex
+++ lib/blog_web/components/layouts.ex
@@ -1,5 +1,15 @@
 defmodule BlogWeb.Layouts do
   use BlogWeb, :html
 
+  import Phoenix.HTML.Tag
+
   embed_templates "layouts/*"
+
+  def meta_tags(attrs_list) do
+    Enum.map(attrs_list, &meta_tag/1)
+  end
+
+  def meta_tag(attrs) do
+    tag(:meta, Enum.into(attrs, []))
+  end
 end
