a66e534647aad24afff43eadd71bcc538f36d157
Aswin Mohan <dev.aswinmohan@gmail.com>
2024-04-25 14:14:32 +0530

    Go with the latest changes and it will work

--- config/config.exs
+++ config/config.exs
@@ -33,7 +33,7 @@ config :blog, Blog.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.20.2",
+  version: "0.17.11",
   blog: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
